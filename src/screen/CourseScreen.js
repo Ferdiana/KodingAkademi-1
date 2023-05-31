4413f37b228d4e09a41d9b8791c0f31e6cd283af
anggiwdntr29 <widiantara2906@gmail.com>
2023-05-31 12:12:19 +0800

    commit

--- src/screen/CourseScreen.js
+++ src/screen/CourseScreen.js
@@ -1,15 +1,31 @@
-import React, {useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Box, Center, Stack, Text} from 'native-base';
-import {AllCourse, SearchBar} from '../components';
+import {AllCourse, CategoryButtons, SearchBar} from '../components';
 import Colors from '../theme/colors';
+import {API_Course} from '../controller/API_Course';
+import {AuthContext} from '../controller/AuthContext';
 
 function CourseScreen() {
   const [searchText, setSearchText] = useState('');
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [courses, setCourses] = useState([]);
+  const {user} = useContext(AuthContext);
 
   const handleSearchTextChange = text => {
     setSearchText(text);
   };
+
+  useEffect(() => {
+    const loadCourses = async () => {
+      if (user.accessToken) {
+        const coursesData = await API_Course(user.accessToken);
+        setCourses(coursesData);
+      }
+    };
+    loadCourses();
+  }, [user.accessToken]);
+
+  const categories = [...new Set(courses.map(course => course.category.name))];
 
   return (
     <Stack flex={1} bg={'neutral.50'}>
@@ -28,6 +44,13 @@ function CourseScreen() {
         placeholder={'Search'}
         onChangeText={handleSearchTextChange}
       />
+      <Stack mt={'10px'}>
+        <CategoryButtons
+          categories={categories}
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+      </Stack>
       <Box my={'5px'} />
       <AllCourse searchText={searchText} selectedCategory={selectedCategory} />
     </Stack>
